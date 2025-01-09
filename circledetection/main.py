ad84fb0967250b7515bad810349366eec96c61cf
mandy wu <149692472+pl1006321@users.noreply.github.com>
2025-01-09 09:45:26 -0500

    Update main.py

--- circledetection/main.py
+++ circledetection/main.py
@@ -1,28 +1,30 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image.png')
+img = cv2.imread('image6.png')
 final = img.copy()
 
 height, width, _ = img.shape
-min_r = round(width/65)
-max_r = round(width/11)
-min_dis = round(width/7)
+# cv2.resize()
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blurred = cv2.blur(gray, (20, 20), cv2.BORDER_DEFAULT)
-circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, min_dis, param1=10, param2=40, minRadius=min_r, maxRadius=max_r, )
+# blurred = cv2.medianBlur(gray, 7)
+gaussian = cv2.GaussianBlur(gray, (15, 15), 0)
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+circles = cv2.HoughCircles(gaussian, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=100)
 
 if circles is not None:
     circles = np.uint16(np.around(circles))
+
     for circle in circles[0, :]:
         h, k, r = circle[0], circle[1], circle[2]
-        cv2.circle(final, (h, k), r, (0, 255, 0), 2)
+        cv2.circle(final, (h, k), r, (255, 0, 255), 2)
         cv2.circle(final, (h, k), 1, (0, 0, 255), -1)
+
 
 cv2.imshow('og', img)
 cv2.imshow('gray', gray)
-cv2.imshow('blurred', blurred)
+cv2.imshow('edges', gaussian)
 cv2.imshow('overlay', final)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
