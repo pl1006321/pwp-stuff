b29e1a693658342f8fe927f6d2d1756d39f7c4b2
mandy wu <149692472+pl1006321@users.noreply.github.com>
2025-01-10 09:45:22 -0500

    Update main.py

--- circledetection/main.py
+++ circledetection/main.py
@@ -1,30 +1,40 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image6.png')
-final = img.copy()
+img = cv2.imread('image3.png')
 
 height, width, _ = img.shape
-# cv2.resize()
+img = cv2.resize(img, (600, int(height*600/width)))
+final = img.copy()
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# blurred = cv2.medianBlur(gray, 7)
-gaussian = cv2.GaussianBlur(gray, (15, 15), 0)
-edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-circles = cv2.HoughCircles(gaussian, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=100)
 
+blurred = cv2.medianBlur(gray, 9)
+gaussian = cv2.GaussianBlur(gray, (9, 9), 0)
+
+# edges = cv2.Canny(gaussian, 50, 150, apertureSize=3)
+
+# kernel = np.ones((5, 5), np.uint8)
+# dilated = cv2.dilate(edges, kernel, iterations=1)
+
+circles = cv2.HoughCircles(gaussian, cv2.HOUGH_GRADIENT, 1, 100, param1=50, param2=100)
+
+count = 0
 if circles is not None:
-    circles = np.uint16(np.around(circles))
-
-    for circle in circles[0, :]:
-        h, k, r = circle[0], circle[1], circle[2]
-        cv2.circle(final, (h, k), r, (255, 0, 255), 2)
+    circles = np.round(circles[0, :]).astype("int")
+    for (h, k, r) in circles:
+        cv2.circle(final, (h, k), r, (255, 0, 255), 4)
         cv2.circle(final, (h, k), 1, (0, 0, 255), -1)
+        count += 1
 
-
+print(f'{count} circle(s) detected')
+print(final.shape)
 cv2.imshow('og', img)
 cv2.imshow('gray', gray)
-cv2.imshow('edges', gaussian)
+cv2.imshow('gaussian', gaussian)
+cv2.imshow('blur', blurred)
+# cv2.imshow('edges', edges)
+# cv2.imshow('dilation', dilated)
 cv2.imshow('overlay', final)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
