3d6f1ebba8f17eb64f82571db6fbb44f0ca76db7
mandy wu <149692472+pl1006321@users.noreply.github.com>
2025-01-24 09:45:20 -0500

    Update main2.py

--- circledetection/main2.py
+++ circledetection/main2.py
@@ -3,33 +3,35 @@ import numpy as np
 
 def detect_circle(frame):
     img = frame.copy()
-    height, width, _ = img.shape
-    img = cv2.resize(img, (600, int(height * 600 / width)))
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    blurred = cv2.medianBlur(gray, 13)
-    gaussian = cv2.GaussianBlur(gray, (15, 15), 0)
+    blurred = cv2.medianBlur(gray, 21)
+    gaussian = cv2.GaussianBlur(gray, (23, 23), 0)
+    bilateral = cv2.bilateralFilter(gray, 15, 200, 200)
 
     # edges = cv2.Canny(gaussian, 50, 150, apertureSize=3)
 
     # kernel = np.ones((5, 5), np.uint8)
     # dilated = cv2.dilate(edges, kernel, iterations=1)
 
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 200, param1=50, param2=100)
+    circles = cv2.HoughCircles(gaussian, cv2.HOUGH_GRADIENT, 1, 250, param1=50, param2=120)
 
     count = 0
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
         for (h, k, r) in circles:
-            cv2.circle(img, (h, k), r, (255, 0, 255), 4)
+            print(r)
+            cv2.circle(img, (h, k), r, (255, 0, 255), 5)
             cv2.circle(img, (h, k), 1, (0, 0, 255), -1)
             count += 1
 
-    return img
+    if 
+        r = None
+    return img, r
 
 
-cap = cv2.VideoCapture('video2.mov')
+cap = cv2.VideoCapture('video6.mov')
 
 # Check if camera opened successfully
 if (cap.isOpened() == False):
@@ -40,7 +42,16 @@ while (cap.isOpened()):
 
     # Capture frame-by-frame
     ret, frame = cap.read()
-    overlay = detect_circle(frame)
+    height, width, _ = frame.shape
+    frame = cv2.resize(frame, (600, int(height * 600 / width)))
+
+    avg = 0
+    frames = 0
+    overlay, r = detect_circle(frame)
+
+    if r is not None:
+        avg += r
+    frames += 1
     if ret == True:
         # Display the resulting frame
         cv2.imshow('og', frame)
@@ -59,3 +70,4 @@ cap.release()
 
 # Closes all the frames
 cv2.destroyAllWindows()
+print(f'average: {avg/frames}')
