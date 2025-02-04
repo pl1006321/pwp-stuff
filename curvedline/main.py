5b62e081976ed4e0bcad747572d74a1e1976fa56
mandy wu <149692472+pl1006321@users.noreply.github.com>
2025-02-04 09:45:30 -0500

    Update main.py

--- curvedline/main.py
+++ curvedline/main.py
@@ -35,50 +35,58 @@ def apply_overlay(frame):
     cropped = img[50:height - 50, 100:width - 100]  # makes a cropped roi
 
     gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)  # make grayscale
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)  # apply gaussian blur
-    edges = cv2.Canny(blurred, 50, 150, apertureSize=3)  # detect edges
+    blurred = cv2.GaussianBlur(gray, (9, 9), 0)  # apply gaussian blur
+    edges = cv2.Canny(blurred, 50, 100, apertureSize=3)  # detect edges
 
     # use morphological closing to close in gaps 
     kernel = np.ones((21, 21), np.uint8)
     closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+    copy = closed.copy()
 
     # lines = cv2.HoughLinesP(closed, rho=1, theta=np.pi/180, threshold=50, minLineLength=30)
     # if lines is not None:
     #     x1, y1, x2, y2 = lines[0][0]
     #     cv2.line(cropped, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
-    contours, _ = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(copy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours_list = list(contours)
     contours_list.sort(key=cv2.contourArea)
 
-    middle_pts = [] 
+    middle_pts = []
 
     if len(contours_list) >= 2:
         con1 = contours_list[-1]
         con2 = contours_list[-2]
 
-        line1 = cv2.approxPolyDP(con1, 2, True)
+        line1 = cv2.approxPolyDP(con1, 8, True)
+        line1 = line1[:int(len(line1)/1.3)]
         cv2.drawContours(cropped, [line1], -1, (255, 0, 0), 3)
 
-        line2 = cv2.approxPolyDP(con2, 2, True)
+        line2 = cv2.approxPolyDP(con2, 8, True)
+        line2 = line2[:int(len(line1)/1.3)]
         cv2.drawContours(cropped, [line2], -1, (255, 0, 0), 3)
 
-        min_len = min(len(line1), len(line2))
-        line1 = line1[:min_len]; line2 = line2[:min_len]
+        for coords1, coords2 in zip(line1, line2):
+            x1, y1 = coords1[0]
+            x2, y2 = coords2[0]
 
-        for i in range(min_len-1):
-            x1, y1 = line1[i][0]
-            x2, y2 = line2[i][0]
-
-            midx = int((x1 + x2)/2); midy = int((y1 + y2)/2)
+            midx = int((x1 + x2) / 2)
+            midy = int((y1 + y2) / 2)
             middle_pts.append([midx, midy])
-            
-    middle_pts = np.array(middle_pts, dtype=np.uint8)
-    if middle_pts: 
+
+            # middle_pts = [[x1, y1], [x2, y2]...]
+
+    middle_pts = np.array(middle_pts, dtype=np.int32)
+
+    if len(middle_pts) > 0:
         cv2.polylines(cropped, [middle_pts], False, (255, 0, 255), 4)
 
     img[50:height - 50, 100:width - 100] = cropped
     cv2.rectangle(img, (100, 50), (width - 100, height - 50), (255, 0, 0), 2)
+
+    cv2.imshow('canny', edges)
+    cv2.imshow('closed', closed)
+    cv2.imshow('contours', copy)
 
     return img
 
