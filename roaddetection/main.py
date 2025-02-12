cbe453aaa7d325e4b5376625a0ea90da1435bde8
mandy wu <149692472+pl1006321@users.noreply.github.com>
2025-02-12 09:44:35 -0500

    Update main.py

--- roaddetection/main.py
+++ roaddetection/main.py
@@ -1,21 +1,52 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('image.png') # change to file path
+def draw_lines(frame):
+    copy = frame.copy()
+    cv2.line(copy, (300, 750), (800, 550), (255, 0, 255), 10)
+    cv2.line(copy, (800, 550), (1100, 550), (0, 0, 255), 10)
+    cv2.line(copy, (1100, 550), (1600, 750), (0, 255, 0), 10)
+    cv2.line(copy, (1600, 750), (300, 750), (255, 0, 0), 10)
+    return copy
 
-# List to store points
-points = []
+def draw_points(frame):
+    copy = frame.copy()
+    cv2.circle(copy, (1400, 1000), 10, (255, 0, 0), -1)
+    cv2.circle(copy, (2000, 1000), 10, (255, 0, 0), -1)
+    cv2.circle(copy, (2500, 1400), 10, (255, 0, 0), -1)
+    cv2.circle(copy, (700, 1400), 10, (255, 0, 0), -1)
+    return copy
 
-cv2.circle(image, (868, 1384), 5, (255, 0, 0), -1)
-cv2.circle(image, (1484, 980), 5, (255, 0, 0), -1)
-cv2.circle(image, (1887, 980), 5, (255, 0, 0), -1)
-cv2.circle(image, (2437, 1384), 5, (255, 0, 0), -1)
+def pers_trans(frame):
+    copy = frame.copy()
+    
+    og_pts = np.float32([[]])
 
-# Load and display the image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+camera = cv2.VideoCapture('video.mp4')
 
-# (868, 1384)
-# (1484, 970)
-# 1887, 980
-# 2437, 1383
+if not camera.isOpened():
+    print('failed to connect')
+    exit()
+
+while True:
+    ret, frame = camera.read()
+
+    if not ret:
+        print('failed to get frame')
+        break
+
+    mask = draw_lines(frame)
+
+    cv2.imshow("og video", frame)
+    print(frame.shape)
+    cv2.imshow('test', mask)
+
+    if cv2.waitKey(1) != -1:
+        break
+
+"""
+top left: (800, 550)
+top right: (1100, 550)
+bottom right: (1600, 750)
+bottom left: (300, 750)
+"""
