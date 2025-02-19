5f394e8abad0e5a945ae2ec4bbb40b687b1f1ab7
mandy wu <149692472+pl1006321@users.noreply.github.com>
2025-02-19 06:39:32 -0500

    Update main.py

--- roaddetection/main.py
+++ roaddetection/main.py
@@ -1,36 +1,42 @@
 import cv2
 import numpy as np
 
+# top left: (0, int(height*3/5))
+# top right: (width, int(height*3/5))
+# bottom right: (width, height)
+# bottom left: (0, height)
+
 def draw_lines(frame):
     copy = frame.copy()
     height, width, _ = copy.shape
-    cv2.line(copy, (0, height-100), (0, int(height*3/5)), (255, 0, 255), 10)
+    cv2.line(copy, (0, height), (0, int(height*3/5)), (255, 0, 255), 10)
     cv2.line(copy, (0, int(height*3/5)), (width, int(height*3/5)), (0, 0, 255), 10)
-    cv2.line(copy, (width, int(height*3/5)), (width, height-100), (0, 255, 0), 10)
-    cv2.line(copy, (width, height-100), (0, height-100), (255, 0, 0), 10)
-    return copy
-
-def draw_points(frame):
-    copy = frame.copy()
-    height, width, _ = copy.shape
-    cv2.circle(copy, (300, 750), 10, (255, 0, 0), -1)
-    cv2.circle(copy, (800, 550), 10, (0, 255, 0), -1)
-    cv2.circle(copy, (1100, 550), 10, (0, 0, 255), -1)
-    cv2.circle(copy, (1600, 750), 10, (255, 0, 255), -1)
+    cv2.line(copy, (width, int(height*3/5)), (width, height), (0, 255, 0), 10)
+    cv2.line(copy, (width, height), (0, height), (255, 0, 0), 10)
     return copy
 
 def pers_trans(frame):
     copy = frame.copy()
+    height, width, _ = copy.shape
 
-    og_pts = np.float32([[800, 550], [1100, 550], [1600, 750], [300, 750]])
-    dst_pts = np.float32([[0, 0], [300, 0], [300, 300], [0, 300]])
+    og_pts = np.float32([[0, int(height*3/5)], [width, int(height*3/5)], [width, height], [0, height]])
+    dst_pts = np.float32([[0, 0], [500, 0], [500, 500], [0, 500]])
 
     matrix = cv2.getPerspectiveTransform(og_pts, dst_pts)
-    warped = cv2.warpPerspective(copy, matrix, (300, 300))
+    warped = cv2.warpPerspective(copy, matrix, (500, 500))
 
     return warped
 
-camera = cv2.VideoCapture('video2.mov')
+def filters(frame):
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    edges = cv2.Canny(blurred, 30, 80, apertureSize=3)
+
+    # cv2.imshow('gray', gray)
+    cv2.imshow('blurred', blurred)
+    cv2.imshow('edged', edges)
+
+camera = cv2.VideoCapture('video.mov')
 
 if not camera.isOpened():
     print('failed to connect')
@@ -45,9 +51,10 @@ while True:
 
     mask = draw_lines(frame)
     warped = pers_trans(frame)
+    filters(warped)
 
-    cv2.imshow("og video", frame)
-    cv2.imshow('mask', mask)
+    # cv2.imshow("og video", frame)
+    # cv2.imshow('mask', mask)
     cv2.imshow('test', warped)
 
     if cv2.waitKey(1) != -1:
